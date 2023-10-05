e2d78c4662a0b4b06feeed1a33bcd943e7923dbb
RifqiNotWibu <rifqifadhillahh@gmail.com>
2023-10-05 12:14:52 +0700

    Added edit profile feature routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Auth\Events\Login;
 use Illuminate\Support\Facades\Route;
 
@@ -8,9 +9,13 @@ Route::get('/login', [LoginController::class, 'index'])->name('login');
 Route::post('login-proses',[LoginController::class, 'login_proses'])->name('login-proses');
 Route::get('logout', [LoginController::class, 'logout'])->name('logoutUser');
 Route::get('/', [HomeController::class, 'dashboard'])->name('dashboard');
-Route::get('/user', [HomeController::class, 'index'])->name('user.index');
 Route::get('/create', [HomeController::class, 'create'])->name('user.create');
 Route::post('/menyimpan', [HomeController::class, 'store'])->name('simpanData');
+// Route::get('/user', [HomeController::class, 'index'])->name('user.index');
+Route::get('/edit-profile', [ProfileController::class, 'editProfile'])->name('edit-profile');
+Route::get('/profile', [ProfileController::class, 'profile'])->name('profile');
+Route::post('/update-profile', [ProfileController::class, 'updateProfile'])->name('update-profile');
+
 
 Route::get('/edit/{id}', [HomeController::class, 'edit'])->name('editData');
 // {} artinya parameter 
